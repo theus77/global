f442b9176e5729b2dcddc89b22b01495b4da4a5e
theus deka <me@theus.be>
2015-09-11 20:28:58 +0200

    updated provisioning script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
-cd /var/www/public
+su vagrant
 ln -s /var/www/public/Config/database.vagrant.php /var/www/public/Config/database.php
 ln -s /var/www/public/Config/core.vagrant.php /var/www/public/Config/core.php
 ln -s /var/www/public/Config/bootstrap.vagrant.php /var/www/public/Config/bootstrap.php
+cd /var/www/public
 composer update
 bower update
 gem install compass
