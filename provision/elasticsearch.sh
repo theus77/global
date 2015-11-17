6281aa1ad1f96eef8409883f4e079f6153d37213
theus deka <me@theus.be>
2015-11-17 22:30:02 +0100

    Working version of the elasticsearch provision script?

--- provision/elasticsearch.sh
+++ provision/elasticsearch.sh
@@ -3,5 +3,7 @@ echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sud
 sudo apt-get update -y
 sudo apt-get install -y default-jre
 sudo apt-get install -y elasticsearch
-sudo bin/plugin install mobz/elasticsearch-head
+sudo /usr/share/elasticsearch/bin/plugin install mobz/elasticsearch-head
 sudo sed -i 's/# network.host: 192.168.0.1/network.host: ::0/i' /etc/elasticsearch/elasticsearch.yml
+sudo update-rc.d elasticsearch defaults 95 10
+sudo /etc/init.d/elasticsearch start
