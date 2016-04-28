be90293e9305727810014f1b2c33d65899c7b3b6
theus deka <me@theus.be>
2016-04-28 23:34:31 +0200

    To publish on AWS

--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-rsync  -rlth -v --force --omit-dir-times --exclude-from=./excluded.list ".//" "theus@theus.be:/opt/web/sites/globalview//"
+rsync  -rlth  -e '/usr/bin/ssh -i /Users/theus/ganir.pem' -v --force --omit-dir-times --exclude-from=./excluded.list ".//" "ec2-user@ec2-54-93-53-225.eu-central-1.compute.amazonaws.com:/opt/sites/global//"
