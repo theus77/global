efbea89a369f71afc42a29a77c87fab2aa0edef8
theus deka <me@theus.be>
2016-11-01 15:14:29 +0100

    disable ajax search in js

--- web/js/globalview.js
+++ web/js/globalview.js
@@ -88,43 +88,43 @@ $(document).ready(function() {
 	 	return split( term ).pop();
    	}	
 
-   $( ".QuickSearchInput" )
-	 // don't navigate away from the field on tab when selecting an item
-	 .bind( "keydown", function( event ) {
-	   if ( event.keyCode === $.ui.keyCode.TAB &&
-		   $( this ).autocomplete( "instance" ).menu.active ) {
-		 event.preventDefault();
-	   }
-	 })
-	 .autocomplete({
-	   source: function( request, response ) {
-		 $.getJSON( $(".QuickSearchInput").data("search-url"), {
-		   q: extractLast( request.term )
-		 }, response );
-	   },
-	   search: function() {
-		 // custom minLength
-		 var term = extractLast( this.value );
-		 if ( term.length < 2 ) {
-		   return false;
-		 }
-	   },
-	   focus: function() {
-		 // prevent value inserted on focus
-		 return false;
-	   },
-	   select: function( event, ui ) {
-		 var terms = split( this.value );
-		 // remove the current input
-		 terms.pop();
-		 // add the selected item
-		 terms.push( ui.item.value );
-		 // add placeholder to get the comma-and-space at the end
-		 terms.push( "" );
-		 this.value = terms.join( ", " );
-		 return false;
-	   }
-	 });
+//   $( ".QuickSearchInput" )
+//	 // don't navigate away from the field on tab when selecting an item
+//	 .bind( "keydown", function( event ) {
+//	   if ( event.keyCode === $.ui.keyCode.TAB &&
+//		   $( this ).autocomplete( "instance" ).menu.active ) {
+//		 event.preventDefault();
+//	   }
+//	 })
+//	 .autocomplete({
+//	   source: function( request, response ) {
+//		 $.getJSON( $(".QuickSearchInput").data("search-url"), {
+//		   q: extractLast( request.term )
+//		 }, response );
+//	   },
+//	   search: function() {
+//		 // custom minLength
+//		 var term = extractLast( this.value );
+//		 if ( term.length < 2 ) {
+//		   return false;
+//		 }
+//	   },
+//	   focus: function() {
+//		 // prevent value inserted on focus
+//		 return false;
+//	   },
+//	   select: function( event, ui ) {
+//		 var terms = split( this.value );
+//		 // remove the current input
+//		 terms.pop();
+//		 // add the selected item
+//		 terms.push( ui.item.value );
+//		 // add placeholder to get the comma-and-space at the end
+//		 terms.push( "" );
+//		 this.value = terms.join( ", " );
+//		 return false;
+//	   }
+//	 });
 
 	 /* simple sidebar */
 	 /*
