07e80b9a8dcef5037d26470eb95c520e09a19501
theus deka <me@theus.be>
2018-01-20 10:38:56 +0100

    Google webmaster account file

--- publish.sh
+++ publish.sh
@@ -1,1 +1,2 @@
+#ssh -i /Users/theus/ganir.pem ec2-user@ec2-54-93-53-225.eu-central-1.compute.amazonaws.com
 rsync  -rlth  -e '/usr/bin/ssh -i /Users/theus/ganir.pem' -v --force --omit-dir-times --exclude-from=./excluded.list ".//" "ec2-user@ec2-54-93-53-225.eu-central-1.compute.amazonaws.com:/opt/sites/global//"
