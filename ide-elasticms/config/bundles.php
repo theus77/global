1241d3197928afff73d3a8f1497a201e38a35efb
Mathieu De Keyzer <me@theus.be>
2026-04-20 13:55:50 +0200

    feat: ems* auto-complete in twig-land

---
+++ ide-elasticms/config/bundles.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+
+    EMS\ClientHelperBundle\EMSClientHelperBundle::class => ['all' => true],
+    EMS\CommonBundle\EMSCommonBundle::class => ['all' => true],
+    EMS\CoreBundle\EMSCoreBundle::class => ['all' => true],
+];
